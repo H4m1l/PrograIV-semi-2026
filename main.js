ad1033d6a225f473036e7f4784d70e25d378f943
h4mil <Usss032024@ugb.edu.sv>
2026-02-17 21:43:09 -0600

    Creacion de la aplicacion academica usando componenetes de vue.js

--- main.js
+++ main.js
@@ -1,118 +1,47 @@
-const { createApp } = Vue;
+const { createApp } = Vue,
+    Dexie = window.Dexie,
+    db = new Dexie("db_academica");
+
 
 createApp({
-    data() {
-        return {
-            accion: "Nuevo",
-            id: 0,
-            alumno: {
-                codigo: "",
-                nombre: "",
-                direccion: "",
-                municipio: "",
-                departamento: "",
-                telefono: "",
-                fechaNacimiento: "",
-                sexo: "",
-                email: ""
-            },
-            buscar: "",
-            alumnos: []
+    components:{
+        alumnos,
+        busqueda_alumnos,
+        materias,
+        busqueda_materias,
+        docentes,
+        busqueda_docentes
+    },
+    data(){
+        return{
+            forms:{
+                alumnos:{mostrar:false},
+                busqueda_alumnos:{mostrar:false},
+                materias:{mostrar:false},
+                busqueda_materias:{mostrar:false},
+                docentes:{mostrar:false},
+                busqueda_docentes:{mostrar:false},
+                matriculas:{mostrar:false},
+                inscripciones:{mostrar:false}
+            }
         }
     },
-    methods: {
-        obtenerAlumnos() {
-            let n = localStorage.length;
-            this.alumnos = [];
-            for (let i = 0; i < n; i++) {
-                let key = localStorage.key(i);
-                if (!isNaN(Number(key))) {
-                    try {
-                        let data = JSON.parse(localStorage.getItem(key));
-                       
-                        if (data.nombre.toUpperCase().includes(this.buscar.toUpperCase()) ||
-                            data.codigo.toUpperCase().includes(this.buscar.toUpperCase())) {
-                            this.alumnos.push(data);
-                        }
-                    } catch (e) {
-                        console.error("Error reading key: " + key, e);
-                    }
-                }
-            }
+    methods:{
+        buscar(ventana, metodo){
+            this.$refs[ventana][metodo]();
         },
-        eliminarAlumno(id, event) {
-            event.stopPropagation();
-            if (confirm("¿Está seguro de eliminar el alumno?")) {
-                localStorage.removeItem(id);
-                this.obtenerAlumnos();
-            }
+        abrirVentana(ventana){
+            this.forms[ventana].mostrar = !this.forms[ventana].mostrar;
         },
-        modificarAlumno(alumno) {
-            this.accion = "Modificar";
-            this.id = alumno.id;
-           
-            this.alumno = { ...alumno };
-        },
-        guardarAlumno() {
-            let datos = {
-                id: this.accion == "Modificar" ? this.id : this.getId(),
-                codigo: this.alumno.codigo,
-                nombre: this.alumno.nombre,
-                direccion: this.alumno.direccion,
-                municipio: this.alumno.municipio,
-                departamento: this.alumno.departamento,
-                telefono: this.alumno.telefono,
-                fechaNacimiento: this.alumno.fechaNacimiento,
-                sexo: this.alumno.sexo,
-                email: this.alumno.email
-            };
-
-            let codigoDuplicado = this.buscarAlumno(datos.codigo);
-            if (codigoDuplicado && this.accion == "Nuevo") {
-                alert("El código del alumno ya existe, pertenece a: " + codigoDuplicado.nombre);
-                return;
-            }
-
-            localStorage.setItem(datos.id, JSON.stringify(datos));
-            this.limpiarFormulario();
-            this.obtenerAlumnos();
-        },
-        getId() {
-            return new Date().getTime();
-        },
-        limpiarFormulario() {
-            this.accion = "Nuevo";
-            this.id = 0;
-            this.alumno = {
-                codigo: "",
-                nombre: "",
-                direccion: "",
-                municipio: "",
-                departamento: "",
-                telefono: "",
-                fechaNacimiento: "",
-                sexo: "",
-                email: ""
-            };
-        },
-        buscarAlumno(codigo = '') {
-            let n = localStorage.length;
-            for (let i = 0; i < n; i++) {
-                let key = localStorage.key(i);
-                if (!isNaN(Number(key))) {
-                    try {
-                        let datos = JSON.parse(localStorage.getItem(key));
-                        if (datos?.codigo && datos.codigo.trim().toUpperCase() == codigo.trim().toUpperCase()) {
-                            return datos;
-                        }
-                    } catch (e) { }
-                }
-            }
-            return null;
+        modificar(ventana, metodo, data){
+            this.$refs[ventana][metodo](data);
         }
     },
-    mounted() {
-        this.obtenerAlumnos();
+    mounted(){
+        db.version(1).stores({
+            "alumnos": "idAlumno, codigo, nombre, direccion, email, telefono",
+            "materias": "idMateria, codigo, nombre, uv",
+            "docentes": "idDocente, codigo, nombre, direccion, email, telefono, escalafon"
+        });
     }
-}
-).mount("#app");
+}).mount("#app");
